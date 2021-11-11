60fff44547c227cf756f6fbb0c44615695ca2a09
Amit Rana <43043638+dstreta47@users.noreply.github.com>
2021-11-11 12:35:07 -0800

    levelorder

---
+++ tree_levelorder.py
@@ -0,0 +1,63 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+newBT = TreeNode('Drinks')
+hot = TreeNode('hot')
+cold = TreeNode('cold')
+newBT.left = hot
+newBT.right = cold
+tea = TreeNode('tea')
+coffee = TreeNode('coffee')
+hot.left = tea
+hot.right = coffee
+
+def preorder(root):
+    if not root:
+        return
+    print(root.data)
+    preorder(root.left) 
+    preorder(root.right)   
+
+def postorder(root):
+    if not root:
+        return
+    preorder(root.left) 
+    preorder(root.right)    
+    print(root.data)  
+
+def inorder(root):
+    if not root:
+        return
+    preorder(root.left) 
+    print(root.data)  
+    preorder(root.right)   
+
+def levelorder(root):
+    if not root:
+        return
+    else:
+        queue = []
+        queue.append(root)
+        while queue:
+            print(queue[0].data)
+            root = queue.pop(0)
+
+            if root.left is not None:
+                queue.append(root.left)
+            if root.right is not None:
+                queue.append(root.right)    
+
+
+
+
+
+
+# preorder(newBT)
+# inorder(newBT)
+# postorder(newBT)
+levelorder(newBT)
+
+
