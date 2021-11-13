37d3416088fef82fdb87eeba497e78e5c3ed2418
Amit Rana <43043638+dstreta47@users.noreply.github.com>
2021-11-12 22:12:36 -0800

    max of tree

---
+++ Max_funct.py
@@ -0,0 +1,52 @@
+import math
+class tree:
+    def __init__(self,data):
+        self.data= data
+        self.left= None
+        self.right= None
+def inorder(root):
+    if root is None:
+        return
+    inorder(root.left)
+    print(root.data)
+    inorder(root.right)    
+def postorder(root):
+    if root is None:
+        return
+    inorder(root.left)
+    inorder(root.right) 
+    print(root.data)    
+def preorder(root):
+    if root is None:
+        return
+    print(root.data) 
+    inorder(root.left)
+    inorder(root.right) 
+
+def treesize(root):
+    if root is None:
+        return 0
+    else:
+        return(1+ treesize(root.left)+ treesize(root.right))
+def maxtree(root):
+    if root==None:
+        return -math.inf
+    else:
+        ls= maxtree(root.left)
+        rs= maxtree(root.right)
+        return max(root.data,ls,rs)
+
+BT = tree(1)
+BTL = tree(2)
+BTR = tree(8)
+BTLL = tree(7)
+BTLR = tree(20)
+BTRL = tree(18)
+BTRR = tree(80)
+BT.left = BTL
+BT.right = BTR
+BT.left.left = BTLL
+BT.left.right = BTLR
+#inorder(BT)
+#print(treesize(BT))
+print(maxtree(BT))
